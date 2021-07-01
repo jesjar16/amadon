f467c825af85a3e1fa847ffe187b032c67f6d4a7
Jessica <jesjar16@gmail.com>
2021-07-01 17:42:12 -0400

    Fixed version

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
+from django.db.models import Sum
 
 def index(request):
     context = {
@@ -9,8 +10,36 @@ def index(request):
 
 def checkout(request):
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    #price_from_form = float(request.POST["price"])
+    id_from_form = request.POST["product_id"]
+    
+    # getting price from product table
+    this_product= Product.objects.get(id=id_from_form)
+    
+    price = this_product.price
+    
+    total_charge = quantity_from_form * price
+    
     print("Charging credit card...")
+    
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    
+    # calculating # of items ordered so far (returns dictionary)
+    total_items = Order.objects.all().aggregate(Sum('quantity_ordered'))
+    total_items = total_items['quantity_ordered__sum']
+    
+    # calculating total charged so far
+    total_charged = Order.objects.all().aggregate(Sum('total_price'))
+    total_charged = total_charged['total_price__sum']   
+    
+    # formatting total_charged
+    total_charged= '{0:.2f}'.format(total_charged) 
+    
+    context = {
+        'total_charge': total_charge,
+        'total_items': total_items,
+        'total_charged': total_charged
+    }
+    
+    #return render(request, "store/checkout.html", context)
+    return redirect("/")
